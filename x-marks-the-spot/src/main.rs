660947a82720b90ff77d1607f251d20d14406279
bloodycrayfish <m1992steen@gmail.com>
2024-07-24 21:54:11 +0200

    random little moving A vs X

--- x-marks-the-spot/src/main.rs
+++ x-marks-the-spot/src/main.rs
@@ -6,24 +6,71 @@ use std::{
 };
 
 fn main() {
-    let width = 50;
-    let height = 12;
-    clear_screen();
-    print_box(width, height);
-    let mut rng = rand::thread_rng();
-    let one_sec = time::Duration::from_secs(1);
     hide_cursor();
+    clear_screen();
+
+    let height = 12;
+    let width = 50;
+    print_box(width, height);
+
+    let mut rng = rand::thread_rng();
+    let one_sec = time::Duration::from_millis(125);
+
+    //player start
+    let mut player_x = 2;
+    let mut player_y = height - 1;
+    let mut player_move_right = true;
     loop {
         let x = rng.gen_range(2..width - 1);
         let y = rng.gen_range(2..height - 1);
         x_marks_the_spot(x, y);
-        thread::sleep(one_sec);
+
+        let x = rng.gen_range(2..width - 1);
+        let y = rng.gen_range(2..height - 1);
+        x_unmarks_the_spot(x, y);
+        (player_move_right, player_x, player_y) =
+            place_player_bounce(player_move_right, player_x, player_y, width);
+
         let input = catch_input();
         if input == "ESC" {
             continue;
         }
+
+        thread::sleep(one_sec);
     }
-    show_cursor();
+    //show_cursor();
+}
+
+fn place_player_bounce(
+    mut direction_right: bool,
+    mut old_x: i32,
+    old_y: i32,
+    width: i32,
+) -> (bool, i32, i32) {
+    clear_location(old_x, old_y);
+    old_x = if direction_right {
+        old_x + 1
+    } else {
+        old_x - 1
+    };
+    if old_x >= width - 1 {
+        old_x = width - 2;
+        direction_right = false;
+    }
+    if old_x <= 1 {
+        old_x = 2;
+        direction_right = true;
+    }
+
+    set_cursor_pos(old_x, old_y);
+    set_fore_color("32".to_string());
+    println!("A");
+    (direction_right, old_x, old_y)
+}
+
+fn clear_location(x: i32, y: i32) {
+    set_cursor_pos(x, y);
+    println!(" ");
 }
 
 fn catch_input() -> std::string::String {
@@ -39,7 +86,26 @@ fn hide_cursor() {
 
 fn x_marks_the_spot(x: i32, y: i32) {
     set_cursor_pos(x, y);
-    println!("X")
+    set_back_color("47".to_string());
+    set_fore_color("31".to_string());
+    println!("X");
+    set_back_color("0".to_string());
+    set_fore_color("0".to_string());
+}
+
+fn x_unmarks_the_spot(x: i32, y: i32) {
+    set_cursor_pos(x, y);
+    set_fore_color("0".to_string());
+    set_back_color("0".to_string());
+    println!(" ");
+}
+
+fn set_fore_color(color: String) {
+    print!("\x1b[1;{}m", color);
+}
+
+fn set_back_color(color: String) {
+    print!("\x1b[1;{}m", color);
 }
 
 fn print_box(width: i32, height: i32) {
